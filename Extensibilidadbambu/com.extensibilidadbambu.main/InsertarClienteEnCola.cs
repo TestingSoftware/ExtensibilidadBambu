7e1afd0f54d81e625ee91bd6af02a6c68ee20133
cooper15 <v.centeno15@gmail.com>
2016-02-26 16:35:50 -0600

    Obtener clientes y guardarlos en la cola

---
+++ Extensibilidadbambu/com.extensibilidadbambu.main/InsertarClienteEnCola.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Extensibilidadbambu.com.extensibilidadbambu.librerias;
+using Extensibilidadbambu.com.extensibilidadbambu.activemq;
+
+
+namespace Extensibilidadbambu.com.extensibilidadbambu.main
+{
+	public class InsertarClienteEnCola
+	{
+		public InsertarClienteEnCola ()
+		{
+		}
+
+		public void Launcher(String cola, String urlNimbus, String itemsAObtenerNimbus)
+		{
+			Rest rest = new Rest();
+			String[] listaClientes = rest.obtener (urlNimbus, itemsAObtenerNimbus);
+
+			ConectorActiveMQ connector = new ConectorActiveMQ ();
+			foreach (String elemento in listaClientes) 
+			{
+				connector.enviarMensaje (cola, elemento);
+			}
+		}
+	}
+}
